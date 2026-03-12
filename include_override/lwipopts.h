37e72629510319ca6ef8dfaddae766a334f33853
vickyleu <yu6564172@gmail.com>
2026-03-12 17:21:36 +0800

    fix: ESP-IDF v5.5.2 compatibility for micro_ros_espidf_component
    - Ensure include dirs exist before idf_component_register check
    - Add execute permission to git_clone_retry.sh
    - Auto-skip source build when pre-built libmicroros.a exists
    - Set LWIP_COMPAT_SOCKETS to 0 (ESP-IDF v5.5+ provides BSD socket
      names via static inline wrappers, compat macros cause conflicts)
    - Make micro_ros_dev build robust with stamp file and set -e
    - Use git_clone_retry.sh for network resilience in micro_ros_dev

--- include_override/lwipopts.h
+++ include_override/lwipopts.h
@@ -4,9 +4,13 @@
 // Pull in ESP-IDF's lwipopts.h, then override socket compat for micro-ROS builds.
 #include_next "lwipopts.h"
 
+// ESP-IDF v5.5+ provides BSD socket names (accept, bind, send, …) via static
+// inline wrappers in its own lwip/sockets.h.  Setting LWIP_COMPAT_SOCKETS to 1
+// enables conflicting #define macros in the core lwIP headers that clash with
+// those wrappers.  Keep it at 0 so only ESP-IDF's wrappers are active.
 #ifdef LWIP_COMPAT_SOCKETS
 #undef LWIP_COMPAT_SOCKETS
 #endif
-#define LWIP_COMPAT_SOCKETS 1
+#define LWIP_COMPAT_SOCKETS 0
 
 #endif
--- libmicroros.mk
+++ libmicroros.mk
@@ -70,6 +70,8 @@ endif
 
 all: $(EXTENSIONS_DIR)/libmicroros.a
 
+DEV_STAMP := $(EXTENSIONS_DIR)/micro_ros_dev/.dev.stamp
+
 clean:
 	rm -rf $(EXTENSIONS_DIR)/libmicroros.a; \
 	rm -rf $(EXTENSIONS_DIR)/include; \
@@ -89,17 +91,19 @@ $(EXTENSIONS_DIR)/esp32_toolchain.cmake: $(EXTENSIONS_DIR)/esp32_toolchain.cmake
 		sed "s/@BUILD_CONFIG_DIR@/$(subst /,\/,$(BUILD_DIR)/config)/g" \
 		> $(EXTENSIONS_DIR)/esp32_toolchain.cmake
 
-$(EXTENSIONS_DIR)/micro_ros_dev/install:
+$(DEV_STAMP):
 	rm -rf micro_ros_dev; \
+	set -e; \
 	mkdir micro_ros_dev; cd micro_ros_dev; \
 	export GIT_HTTP_VERSION=HTTP/1.1; \
-	git clone -b jazzy https://github.com/ament/ament_cmake src/ament_cmake; \
-	git clone -b jazzy https://github.com/ament/ament_lint src/ament_lint; \
-	git clone -b jazzy https://github.com/ament/ament_package src/ament_package; \
-	git clone -b jazzy https://github.com/ament/googletest src/googletest; \
-	git clone -b jazzy https://github.com/ros2/ament_cmake_ros src/ament_cmake_ros; \
-	git clone -b jazzy https://github.com/ament/ament_index src/ament_index; \
-	colcon build --cmake-args -DBUILD_TESTING=OFF -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=gcc;
+	$(EXTENSIONS_DIR)/scripts/git_clone_retry.sh https://github.com/ament/ament_cmake jazzy src/ament_cmake; \
+	$(EXTENSIONS_DIR)/scripts/git_clone_retry.sh https://github.com/ament/ament_lint jazzy src/ament_lint; \
+	$(EXTENSIONS_DIR)/scripts/git_clone_retry.sh https://github.com/ament/ament_package jazzy src/ament_package; \
+	$(EXTENSIONS_DIR)/scripts/git_clone_retry.sh https://github.com/ament/googletest jazzy src/googletest; \
+	$(EXTENSIONS_DIR)/scripts/git_clone_retry.sh https://github.com/ros2/ament_cmake_ros jazzy src/ament_cmake_ros; \
+	$(EXTENSIONS_DIR)/scripts/git_clone_retry.sh https://github.com/ament/ament_index jazzy src/ament_index; \
+	colcon build --cmake-args -DBUILD_TESTING=OFF; \
+	touch $(DEV_STAMP)
 
 $(EXTENSIONS_DIR)/micro_ros_src/src:
 	rm -rf micro_ros_src; \
@@ -166,7 +170,7 @@ $(EXTENSIONS_DIR)/micro_ros_src/src:
 	test -f src/extra_packages/extra_packages.repos && cd src/extra_packages && vcs import --input extra_packages.repos || :;
 
 
-$(INSTALL_STAMP): $(EXTENSIONS_DIR)/esp32_toolchain.cmake $(EXTENSIONS_DIR)/micro_ros_dev/install $(EXTENSIONS_DIR)/micro_ros_src/src | $(PRE_CLEAN)
+$(INSTALL_STAMP): $(EXTENSIONS_DIR)/esp32_toolchain.cmake $(DEV_STAMP) $(EXTENSIONS_DIR)/micro_ros_src/src | $(PRE_CLEAN)
 	cd $(UROS_DIR); \
 	unset AMENT_PREFIX_PATH; \
 	PATH="$(subst /opt/ros/$(ROS_DISTRO)/bin,,$(PATH))"; \
